16eb523b50d6295b69d3287b38595d422cca6b8c
Silviu <cristian.bunduc28@gmail.com>
2026-02-05 08:47:59 +0200

    Fix sequelize config module

--- config/index.js
+++ config/index.js
@@ -1,10 +1,11 @@
+require('dotenv').config({ quiet: true })
 const { Sequelize } = require('sequelize')
 
 const sequelize = new Sequelize({
   dialect: 'postgres',
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
-  password: process.env.PASSWORD,
+  password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
 })
 
