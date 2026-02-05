df88b82b39963bb89c45c9b3233588a34e2681b6
Silviu <cristian.bunduc28@gmail.com>
2026-02-05 15:26:24 +0200

    Add migration and execute

--- util/db.js
+++ util/db.js
@@ -1,11 +1,28 @@
 const { Sequelize } = require('sequelize')
 const config = require('./config')
+const { Umzug, SequelizeStorage } = require('umzug')
 
 const sequelize = new Sequelize(config)
+
+const runMigrations = async () => {
+  const migrator = new Umzug({
+    migrations: {
+      glob: 'migrations/*.js',
+    },
+    storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+    logger: console,
+  })
+
+  const migrations = await migrator.up()
+  console.log('[INFO] Migrations up to date', {
+    files: migrations.map(mig => mig.name),
+  })
+}
 
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate()
+    await runMigrations()
     console.log('[INFO] Connected to the database')
   } catch (error) {
     console.log('[ERROR] Failed to connect to the database: ', error)
